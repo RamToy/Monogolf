955ed70fb74efb35f892a77f7571d3d8062f757a
RamToy <ramtoy.evevol@yandex.ru>
2018-02-17 20:13:26 +0300

    Add GUI class

---
+++ GUI.py
@@ -0,0 +1,108 @@
+import pygame
+
+
+class GUI:
+    def __init__(self):
+        self.elements = []
+
+    def add_element(self, element):
+        self.elements.append(element)
+
+    def render(self, surface):
+        for element in self.elements:
+            render = getattr(element, "render", None)
+            if callable(render):
+                element.render(surface)
+
+    def update(self):
+        for element in self.elements:
+            update = getattr(element, "update", None)
+            if callable(update):
+                element.update()
+
+    def get_event(self, event):
+        for element in self.elements:
+            get_event = getattr(element, "get_event", None)
+            if callable(get_event):
+                element.get_event(event)
+
+
+class Label:
+    def __init__(self, rect, rect_color, text, font_color):
+        self.rect = pygame.Rect(rect)
+        self.text = text
+        self.rect_color = rect_color
+        self.font_color = font_color
+        self.font = pygame.font.Font(None, self.rect.height - 4)
+        self.rendered_text = None
+        self.rendered_rect = None
+
+    def render(self, surface):
+        if self.rect_color != -1:
+            surface.fill(self.rect_color, self.rect)
+        self.rendered_text = self.font.render(self.text, 1, self.font_color)
+        self.rendered_rect = self.rendered_text.get_rect(x=self.rect.x + 2, centery=self.rect.centery, width=2)
+        surface.blit(self.rendered_text, self.rendered_rect)
+
+
+class Button(Label):
+    def __init__(self, rect, rect_color, text, font_color):
+        super().__init__(rect, rect_color, text, font_color)
+        self.pressed = False
+
+    def render(self, surface):
+        surface.fill(self.rect_color, self.rect)
+        self.rendered_text = self.font.render(self.text, 1, self.font_color)
+        if not self.pressed:
+            color1 = pygame.Color("white")
+            color2 = pygame.Color("black")
+            self.rendered_rect = self.rendered_text.get_rect(x=self.rect.x + 5, centery=self.rect.centery)
+        else:
+            color1 = pygame.Color("black")
+            color2 = pygame.Color("white")
+            self.rendered_rect = self.rendered_text.get_rect(x=self.rect.x + 7, centery=self.rect.centery + 2)
+
+        pygame.draw.rect(surface, color1, self.rect, 2)
+        pygame.draw.line(surface, color2, (self.rect.right - 1, self.rect.top),
+                         (self.rect.right - 1, self.rect.bottom), 2)
+        pygame.draw.line(surface, color2, (self.rect.left, self.rect.bottom - 1),
+                         (self.rect.right, self.rect.bottom - 1), 2)
+        surface.blit(self.rendered_text, self.rendered_rect)
+
+    def get_event(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+            self.pressed = self.rect.collidepoint(*event.pos)
+        elif event.type == pygame.MOUSEBUTTONUP and event.button == 1:
+            self.pressed = False
+            
+            
+class TextBox(Label):
+    def __init__(self, rect, rect_color, text, font_color):
+        super().__init__(rect, rect_color, text, font_color)
+        self.active = True
+        self.blink = True
+        self.blink_timer = 0
+
+    def get_event(self, event):
+        if event.type == pygame.KEYDOWN and self.active:
+            if event.key in (pygame.K_RETURN, pygame.K_KP_ENTER):
+                self.execute()
+            elif event.key == pygame.K_BACKSPACE:
+                if len(self.text) > 0:
+                    self.text = self.text[:-1]
+            else:
+                self.text += event.unicode
+        elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+            self.active = self.rect.collidepoint(event.pos)
+
+    def update(self):
+        if pygame.time.get_ticks() - self.blink_timer > 200:
+            self.blink = not self.blink
+            self.blink_timer = pygame.time.get_ticks()
+
+    def render(self, surface):
+        super(TextBox, self).render(surface)
+        if self.blink and self.active:
+            pygame.draw.line(surface, self.rect_color,
+                             (self.rendered_rect.right + 2, self.rendered_rect.top + 2),
+                             (self.rendered_rect.right + 2, self.rendered_rect.bottom - 2))

4a0dcb3896d990bc4eeded1c7d4dd47971df8bfa
Ilya <Ilya23like73@yandex.ru>
2018-02-23 11:06:25 +0300

    Add comments ad some new images

--- MonoGolf.py
+++ MonoGolf.py
@@ -13,6 +13,7 @@ from Monogolf.GUI import GUI, Label, Button, ImageButton, TextBox
 locked_widget = 140, 140, 140
 coin_color = 255, 219, 77
 
+# Создание экземпляра картинки "заблокированной раскраски"
 pygame.font.init()
 blocked_image = pygame.Surface((110, 60))
 blocked_image.fill(locked_widget)
@@ -22,44 +23,65 @@ blocked_image.blit(font, (25, 8))
 
 
 def create_image_list():
+    """ Создание картинок с сочетаниями цветов """
     imgs = []
     for color_set in colors:
         bg, fg, b, nbg, nfg = color_set
         img = pygame.Surface((110, 60))
         img.fill(bg)
         pygame.draw.circle(img, b, (55, 30), 15, 0)
+        # Значения добавляются в парах: [картинка] - [флаг разблокированности]
         imgs.append([img, True if not imgs else False])
+    # 2 лика величайших
     imgs += [[ilya, False], [artem, False]]
     return imgs
 
 
 def terminate():
+    """ Выход из программы """
     pygame.quit()
     sys.exit()
 
 
 class Game:
+    """ Основной класс игры """
+
     def __init__(self, surface):
+        # Основной холст
         self.surface = surface
+        # Картинки раскрасок
         self.images = create_image_list()
-        self.all_levels = [[level_list[i], True if i == 0 else False] for i in range(len(level_list))]
+        # Уровни представляют собой пары значений (аналогично create_image_list)
+        self.all_levels = [[level_list[i], True if i == 9 else False] for i in range(len(level_list))]
+        # Текущий уровень
         self.current_level = 0
+        # Текущая раскраска
         self.current_design = 0
+        # Текущая картинка шарика
         self.current_image = None
+        # Текущие цвета
         self.bg_color, self.fg_color, self.ball_color, \
             self.n_bg_color, self.n_fg_color = colors[0]
+        # Золото
         self.coins = 10
-        self.lives = 30
+        # Жизни
+        self.lives = 1
 
     def buy_or_select_design(self, index):
+        """ Обработка взаимодействий с конпками магазина """
+        # Если раскраска не разблокирована и надо ее купить
         if not self.images[index][1]:
+            # Проверка и изъятие денежных средств
             if self.coins >= 10:
                 self.coins -= 10
+                # Разблокировка
                 self.images[index][1] = True
+                # Новая раскраска - теперь текущая раскраска
                 self.current_design = index
                 self.bg_color, self.fg_color, self.ball_color, \
                     self.n_bg_color, self.n_fg_color = colors[index]
                 return True
+        # Если раскраска уже разблокированна и надо ее выбрать
         elif self.current_design != index:
             self.current_design = index
             self.bg_color, self.fg_color, self.ball_color,\
@@ -68,11 +90,13 @@ class Game:
         return False
 
     def check_lives(self):
+        """ Проверка жизней """
         self.lives -= 1
         if self.lives <= 0:
             self.lose_page()
 
     def menu_page(self):
+        """ Главное меню """
         gui_menu = GUI()
         gui_menu.add_element(Label((270, 30, 150, 100), -1, "mono", self.fg_color))
         gui_menu.add_element(Label((450, 30, 100, 100), -1, "GOLF", self.ball_color))
@@ -110,6 +134,7 @@ class Game:
                 terminate()
 
     def levels_page(self):
+        """ Страница выбора уровня """
         gui_levels = GUI()
         lvl1 = Button((175, 30, 250, 72), self.fg_color if self.all_levels[0][1] else locked_widget,
                       " Level 1", self.n_bg_color)
@@ -191,6 +216,7 @@ class Game:
                 self.game_page(level_10)
 
     def store_page(self):
+        """ Магазин """
         gui_shop = GUI()
         gui_shop.add_element(Label((300, 20, 310, 100), self.ball_color, " Магазин", self.fg_color))
         gui_shop.add_element(Label((640, 40, 100, 60), -1, " Золото : {}".format(self.coins), coin_color))
@@ -303,22 +329,23 @@ class Game:
             elif image_button2.pressed and self.images[11][1]:
                 self.current_image = None if self.current_image == artem else artem
                 self.store_page()
-            elif text_box1.password == '1337' and text_box1.done and not self.images[10][1]:
+            elif text_box1.password == "1337" and text_box1.done and not self.images[10][1]:
                 self.images[10][1] = True
-                self.current_image = ilya
+                self.current_image = ilya           # Защита 10/10
                 self.store_page()
-            elif text_box2.password == '1247' and text_box2.done and not self.images[11][1]:
+            elif text_box2.password == "1247" and text_box2.done and not self.images[11][1]:
                 self.images[11][1] = True
                 self.current_image = artem
                 self.store_page()
 
     def guide_page(self):
+        """ Информация """
         gui_guide = GUI()
         back = Button((365, 620, 170, 60), self.fg_color, "  назад", self.n_bg_color)
         gui_guide.add_element(back)
 
         self.surface.fill(self.bg_color)
-        self.surface.blit(load_image('guide.png'), (0, 0))
+        self.surface.blit(guide, (0, 0))
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -334,6 +361,7 @@ class Game:
                 self.menu_page()
 
     def developers_page(self):
+        """ Страница разработчиков """
         gui_dev = GUI()
         gui_dev.add_element(Label((70, 70, 200, 60), -1,
                                   "Эта ссылка временно не работает...", self.ball_color))
@@ -359,20 +387,22 @@ class Game:
                 self.menu_page()
 
     def game_page(self, level):
+        """ Основной игровой цикл """
         gui_game = GUI()
-        gui_game.add_element(Label((40, 620, 70, 50), -1, 'жизни : {}'.format(self.lives), self.fg_color))
-        gui_game.add_element(Label((40, 570, 70, 50), -1, 'уровень {}'.format(self.current_level + 1), self.ball_color))
+        gui_game.add_element(Label((40, 620, 70, 50), -1, "жизни : {}".format(self.lives), self.fg_color))
+        gui_game.add_element(Label((40, 570, 70, 50), -1, "уровень {}".format(self.current_level + 1), self.ball_color))
         pause = Button((835, 635, 40, 40), self.n_fg_color, " II", self.n_bg_color)
 
         gui_game.add_element(pause)
 
         clock = pygame.time.Clock()
         slingshot = Slingshot((WIDTH // 2, HEIGHT // 4 * 3), 100, 10, self.fg_color)
+        # Проверка изображений
         if self.current_image is None:
             Ball(slingshot, 15, self.ball_color)
         else:
             Ball(slingshot, 15, self.ball_color, self.current_image)
-
+        # Вызов уровня
         level(self.fg_color, self.ball_color)
 
         running = True
@@ -386,10 +416,10 @@ class Game:
                 slingshot.get_event(event)
 
             ball.update()
-
-            gui_game.elements[0].text = 'жизни : {}'.format(self.lives)
-            gui_game.elements[1].text = 'уровень {}'.format(self.current_level + 1)
-
+            # Обновление показателей
+            gui_game.elements[0].text = "жизни : {}".format(self.lives)
+            gui_game.elements[1].text = "уровень {}".format(self.current_level + 1)
+            # Отрисовка
             self.surface.fill(self.bg_color)
             slingshot.render(self.surface)
             rects.draw(self.surface)
@@ -399,31 +429,33 @@ class Game:
             pygame.display.flip()
 
             clock.tick(30 + ball.sprite.speed * 3)
-
+            # Обработка перезапуска
             if slingshot.restart:
                 slingshot.restart = False
                 self.check_lives()
-
+            # Обработка попадания
             elif ball.sprite.hitting:
                 self.coins += 10
                 self.page_between_levels()
-
+            # Обработка паузы
             if pause.pressed:
                 self.pause_page()
                 pause.pressed = False
 
     def page_between_levels(self):
+        """ Страница при переходе между уровнями """
         self.current_level += 1
         if self.current_level >= 10:
             self.won_page()
         self.all_levels[self.current_level][1] = True
 
+        # Очистка старого поля
         rects.empty()
         hole.empty()
 
         gui_lvl = GUI()
         gui_lvl.add_element(Label((150, 50, 620, 100), self.fg_color, " Уровень пройден", self.n_bg_color))
-        gui_lvl.add_element(Label((300, 170, 350, 300), -1, random.choice(['GG!', 'GJ!', 'WP!']), self.ball_color))
+        gui_lvl.add_element(Label((300, 170, 350, 300), -1, random.choice(["GG!", "GJ!", "WP!"]), self.ball_color))
         gui_lvl.add_element(Label((110, 430, 300, 90), -1, "Золото + 10   Всего : {}".format(self.coins),
                                   self.ball_color if self.current_design == 9 else coin_color))
         go_to_menu = Button((100, 550, 260, 100), self.fg_color, "В меню", self.n_bg_color)
@@ -448,6 +480,7 @@ class Game:
                 self.game_page(self.all_levels[self.current_level][0])
 
     def pause_page(self):
+        """ Пауза """
         gui_pause = GUI()
         gui_pause.add_element(Label((375, 210, 160, 70), self.n_fg_color, " Пауза", self.n_bg_color))
         gui_pause.add_element(Label((300, 280, 150, 70), -1, "Начать заново", self.n_bg_color))
@@ -472,21 +505,25 @@ class Game:
             pygame.display.flip()
 
             if go_to_menu.pressed:
+                # Если игрок выходит в меню, то отнимается 1 жизнь
                 self.check_lives()
                 self.menu_page()
             elif back.pressed:
                 break
 
     def lose_page(self):
+        """ Проигрыш """
         gui_over = GUI()
-        gui_over.add_element(Label((200, 30, 540, 100), self.ball_color, " Вы проиграли!", self.n_bg_color))
-        gui_over.add_element(Label((150, 180, 540, 50), -1, "К сожалению, у Вас закончились жизни", self.n_bg_color))
-        gui_over.add_element(Label((120, 230, 540, 50), -1, "Можете начать новую игру, перейдя в меню", self.n_bg_color))
-        gui_over.add_element(Label((140, 360, 540, 60), -1, "[ Прогресс покупок сохраняется ]", self.ball_color))
+        gui_over.add_element(Label((200, 30, 540, 100), self.ball_color, " Вы проиграли!", self.fg_color))
+        gui_over.add_element(Label((150, 180, 540, 50), -1, "К сожалению, у Вас закончились жизни", self.fg_color))
+        gui_over.add_element(Label((120, 230, 540, 50), -1,
+                                   "Можете начать новую игру, перейдя в меню", self.fg_color))
+        gui_over.add_element(Label((140, 310, 540, 60), -1, "[ Прогресс покупок сохраняется ]", self.ball_color))
         go_to_menu = Button((100, 550, 260, 100), self.fg_color, "В меню", self.n_bg_color)
 
         gui_over.add_element(go_to_menu)
 
+        # Игра начинается заново (игровые показатели обнуляются)
         self.all_levels = [[level_list[i], True if i == 0 else False] for i in range(len(level_list))]
         self.lives = 30
         self.current_level = 0
@@ -494,6 +531,7 @@ class Game:
         hole.empty()
 
         self.surface.fill(self.bg_color)
+        screen.blit(aquaman, (550, 370))
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -507,20 +545,23 @@ class Game:
                 self.menu_page()
 
     def won_page(self):
+        """ Победа """
         gui_won = GUI()
-        gui_won.add_element(Label((200, 30, 540, 100), self.ball_color, " Вы победили!", self.n_bg_color))
-        gui_won.add_element(Label((230, 180, 540, 50), -1, "Поздравляем, Вы лучше всех!", self.n_bg_color))
-        gui_won.add_element(Label((130, 230, 540, 50), -1, "Можете продолжить игру, перейдя в меню", self.n_bg_color))
+        gui_won.add_element(Label((200, 30, 540, 100), self.ball_color, " Вы победили!", self.fg_color))
+        gui_won.add_element(Label((230, 180, 540, 50), -1, "Поздравляем, Вы лучше всех!", self.fg_color))
+        gui_won.add_element(Label((130, 230, 540, 50), -1, "Можете продолжить игру, перейдя в меню", self.fg_color))
         gui_won.add_element(Label((140, 360, 540, 60), -1, "[ Прогресс покупок сохраняется ]", self.ball_color))
         go_to_menu = Button((100, 550, 260, 100), self.fg_color, "В меню", self.n_bg_color)
 
         gui_won.add_element(go_to_menu)
 
+        # Надо вставить сюда ачивку
         self.lives = 1000
         rects.empty()
         hole.empty()
 
         self.surface.fill(self.bg_color)
+        screen.blit(wildsponge, (450, 440))
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -536,15 +577,15 @@ class Game:
 
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption('MonoGolf')
+pygame.display.set_caption("MonoGolf")
 
 
 def load_image(name, colorkey=None):
-    fullname = os.path.join('data', name)
+    fullname = os.path.join("data", name)
     try:
         image = pygame.image.load(fullname)
     except pygame.error as err:
-        print('Cannot load file', name)
+        print("Cannot load file", name)
         raise SystemExit(err)
     image = image.convert_alpha()
     if colorkey is not None:
@@ -554,9 +595,12 @@ def load_image(name, colorkey=None):
     return image
 
 
-pygame.display.set_icon(load_image('MG.png'))
-artem = load_image('Artem.png')
-ilya = load_image('Ilya.png')
+pygame.display.set_icon(load_image("MG.png"))
+guide = load_image("guide.png")
+artem = load_image("Artem.png")
+ilya = load_image("Ilya.png")
+aquaman = load_image("aquaman.png")
+wildsponge = load_image("wildsponge.png")
 
 new_game = Game(screen)
 new_game.menu_page()
